79538252f413538ea506b891daf049a3873f6a93
Neel Karn <neelmani.karn@trantorinc.com>
2016-11-24 17:44:00 +0530

    initial import

---
+++ app/src/main/java/com/neel/firebasenotifications/MainActivity.java
@@ -0,0 +1,269 @@
+package com.neel.firebasenotifications;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.neel.firebasenotifications.model.User;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "FirebaseIDService";
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private Button button2;
+    private EditText name, email;
+
+
+    /**
+     * test firebase database
+     */
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+    private String userId;
+    private boolean isExists = false;
+    /**
+     * adding signing method
+     */
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+
+        // get reference to 'users' node
+        mFirebaseDatabase = mFirebaseInstance.getReference("users");
+
+        // store app title to 'app_title' node
+        mFirebaseInstance.getReference("app_title").setValue("Realtime Databas");
+
+        // app_title change listener
+        mFirebaseInstance.getReference("app_title").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.e(TAG, "App title updated");
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.e(TAG, "Failed to read app title value.", error.toException());
+            }
+        });
+
+        email = (EditText) findViewById(R.id.editText2);
+        name = (EditText) findViewById(R.id.editText);
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isExists = false;
+                userId = "";
+
+                // Firebase analytic
+                /*Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "1");
+                bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "neel");
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+*/
+
+                // Firebase signInAnonymously
+                /*
+                mAuth.signInAnonymously()
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Log.w(TAG, "signInAnonymously", task.getException());
+                                    Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+*/
+                String nameString = name.getText().toString();
+                String emailString = email.getText().toString();
+
+                //Firebase database integration
+                mFirebaseDatabase.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Log.e("Count ", "" + dataSnapshot.getChildrenCount());
+
+                        for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                            User post = postSnapshot.getValue(User.class);
+
+                            Log.e("Get Data", post.name);
+
+                            if (post.email.equals(email.getText().toString().trim())) {
+                                userId = postSnapshot.getKey();
+                                updateUser(name.getText().toString().trim(), email.getText().toString().trim());
+                                isExists = true;
+                                break;
+                            }
+                        }
+
+                        if (!isExists) {
+                            createUser(name.getText().toString().trim(), email.getText().toString().trim());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+
+                });
+            }
+        });
+
+
+
+        /*if (checkPlayServices()) {
+            Intent intent = new Intent(MainActivity.this, FirebaseIDService.class);
+            startService(intent);
+
+            //String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+            //Log.d(TAG, "Refreshed token: " + refreshedToken);
+        }*/
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    public boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, 1001).show();
+            } else {
+                //Log.i(TAG, "This device is not supported.");
+                //finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    /**
+     * Creating new user node under 'users' in firebase database
+     *
+     * @param name
+     * @param email
+     */
+    private void createUser(String name, String email) {
+        // TODO
+        // In real apps this userId should be fetched
+        // by implementing firebase auth
+        if (TextUtils.isEmpty(userId)) {
+            userId = mFirebaseDatabase.push().getKey();
+        }
+        User user = new User(name, email);
+        mFirebaseDatabase.child(userId).setValue(user);
+    }
+
+    /**
+     * User data change listener
+     */
+    private void addUserChangeListener() {
+        // User data change listener
+        mFirebaseDatabase.child(userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.e("getChildren()", dataSnapshot.getChildren().toString());
+                User user = dataSnapshot.getValue(User.class);
+
+                // Check for null
+                if (user == null) {
+                    Log.e(TAG, "User data is null!");
+                    return;
+                }
+                isExists = false;
+                Log.e(TAG, "User data is changed!" + user.name + ", " + user.email);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.e(TAG, "Failed to read user", error.toException());
+            }
+        });
+    }
+
+
+    /**
+     * update a user node under 'users' in firebase database
+     *
+     * @param name
+     * @param email
+     */
+    private void updateUser(String name, String email) {
+        // updating the user via child nodes
+        if (!TextUtils.isEmpty(name))
+            mFirebaseDatabase.child(userId).child("name").setValue(name);
+
+        if (!TextUtils.isEmpty(email))
+            mFirebaseDatabase.child(userId).child("email").setValue(email);
+        isExists = false;
+    }
+}
